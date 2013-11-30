c9ced2bf774ab67c624284ac020bccc2d55b49f0
Chris Choi <chutsu@gmail.com>
2013-11-30 17:07:16 +0000

    Add FunctionRegistry implementation and tests

--- playground/functions.py
+++ playground/functions.py
@@ -1,5 +1,48 @@
 #!/usr/bin/env python
+import math
+
+
+def add_function(left, right):
+    return left + right
+
+
+def sub_function(left, right):
+    return left - right
+
+
+def mul_function(left, right):
+    return left * right
+
+
+def div_function(left, right):
+    return left / right
+
+
+def cos_function(value):
+    return math.cos(value)
+
+
+def sin_function(value):
+    return math.sin(value)
 
 
 class FunctionRegistry(object):
-    print "HELLO"
+    def __init__(self, override_defaults=False):
+        self.functions = {}
+
+        if override_defaults is False:
+            self.register("ADD", add_function)
+            self.register("SUB", sub_function)
+            self.register("MUL", mul_function)
+            self.register("DIV", div_function)
+            self.register("COS", cos_function)
+            self.register("SIN", sin_function)
+
+    def register(self, function_name, function):
+        self.functions[function_name] = function
+
+    def unregister(self, function_name):
+        del self.functions[function_name]
+
+    def get_function(self, function_name):
+        return self.functions[function_name]
---
+++ tests/functions_tests.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+import sys
+import os
+import unittest
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+import playground.config as config
+from playground.functions import FunctionRegistry
+
+# SETTINGS
+config_fp = os.path.join(os.path.dirname(__file__), "config/initializer.json")
+
+
+class InitializerTests(unittest.TestCase):
+    def setUp(self):
+        self.config_file = config.load_config(config_fp)
+        self.function = FunctionRegistry()
+
+    def tearDown(self):
+        del self.config_file
+
+    def test_get_function(self):
+        add = self.function.get_function("ADD")
+        result = add(1, 2)
+        self.assertEquals(result, 3)
+
+        sub = self.function.get_function("SUB")
+        result = sub(2, 1)
+        self.assertEquals(result, 1)
+
+        mul = self.function.get_function("MUL")
+        result = mul(2, 1)
+        self.assertEquals(result, 2)
+
+        div = self.function.get_function("DIV")
+        result = div(4, 2)
+        self.assertEquals(result, 2)
+
+        cos = self.function.get_function("COS")
+        result = cos(0)
+        self.assertEquals(result, 1)
+
+        sin = self.function.get_function("SIN")
+        result = sin(0)
+        self.assertEquals(result, 0)
+
+if __name__ == '__main__':
+    unittest.main()

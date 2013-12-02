ff5a8cbbc7f6d86b6b24b81a0774ff2ada0cdc83
Chris Choi <chutsu@gmail.com>
2013-12-02 11:18:58 +0000

    Refactor crossover to be more diverse
    Instead of a uniform crossover index, the crossover index are not
    guaranteed to be uniform, else the trees which result from this
    operation may hinder diversity, the trees could end up having the same
    shapes.

--- playground/operators/crossover.py
+++ playground/operators/crossover.py
@@ -1,10 +1,13 @@
 #1/usr/bin/env python
 from random import randint
+from random import random
 
 
 class TreeCrossover(object):
+    def __init__(self, config):
+        self.config = config
 
-    def _symetric_crossover_index(self, tree_1, tree_2):
+    def _crossover_index(self, tree_1, tree_2):
         t_1_len = len(tree_1.func_nodes)
         t_2_len = len(tree_1.func_nodes)
 
@@ -17,15 +20,22 @@ class TreeCrossover(object):
 
         return randint(start, end)
 
-    def one_point_crossover(self, tree_1, tree_2, crossover_index=None):
-        # check crossover index
-        if crossover_index is None:
-            crossover_index = self._symetric_crossover_index(tree_1, tree_2)
-
+    def point_crossover(self, tree_1, tree_2, crossover_index=None):
         # get the nodes to swap
-        func_node_1 = tree_1.func_nodes[crossover_index]
-        func_node_2 = tree_2.func_nodes[crossover_index]
+        func_node_1 = tree_1.func_nodes[self._crossover_index(tree_1, tree_2)]
+        func_node_2 = tree_2.func_nodes[self._crossover_index(tree_1, tree_2)]
 
         # swap
         tree_1.replace_node(func_node_1, func_node_2)
         tree_2.replace_node(func_node_2, func_node_1)
+
+    def crossover(self, tree_1, tree_2):
+        method = self.config["crossover"]["method"]
+        crossover_prob = self.config["crossover"]["probability"]
+        prob = random()
+
+        if crossover_prob >= prob:
+            if method == "POINT_CROSSOVER":
+                self.point_crossover(tree_1, tree_2)
+            else:
+                raise RuntimeError("Undefined crossover method!")

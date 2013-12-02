0b7cf8a20fa02c114a03feb19ccd729945f73865
Chris Choi <chutsu@gmail.com>
2013-12-02 11:20:10 +0000

    Temp fix for mutation
    There was a problem when mutating INPUT nodes, in that if there is only
    1 input node, the mutation method would loop forever trying to find
    another input node that does not exist.

--- playground/operators/mutation.py
+++ playground/operators/mutation.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from random import randint
+from random import random
 
 from playground.tree import TreeNodeType
 
@@ -10,7 +11,7 @@ class TreeMutation(object):
 
     def _get_new_node(self, node):
         t = node.node_type
-        if (t == TreeNodeType.UNARY_OP or t == TreeNodeType.BINARY_OP):
+        if t == TreeNodeType.UNARY_OP or t == TreeNodeType.BINARY_OP:
             config_index = "function_nodes"
         elif t == TreeNodeType.TERM:
             config_index = "terminal_nodes"
@@ -20,17 +21,17 @@ class TreeMutation(object):
         index = randint(0, len(self.config[config_index]) - 1)
         new_node = self.config[config_index][index]
 
-        loop = True
-        while loop:
-            index = randint(0, len(self.config[config_index]) - 1)
-            new_node = self.config[config_index][index]
+        # loop = True
+        # while loop:
+        #     index = randint(0, len(self.config[config_index]) - 1)
+        #     new_node = self.config[config_index][index]
 
-            if new_node["type"] == TreeNodeType.TERM:
-                if new_node["type"] == t and new_node["value"] != node.value:
-                    loop = False
-            else:
-                if new_node["type"] == t and new_node["name"] != node.name:
-                    loop = False
+        #     if new_node["type"] == TreeNodeType.TERM:
+        #         if new_node["type"] == t and new_node["value"] != node.value:
+        #             loop = False
+        #     else:
+        #         if new_node["type"] == t and new_node["name"] != node.name:
+        #             loop = False
 
         return new_node
 
@@ -49,8 +50,19 @@ class TreeMutation(object):
             node.name = new_node["name"]
         elif node.node_type == TreeNodeType.TERM:
             new_node = self._get_new_node(node)
-            node.name = new_node["name"]
+            node.name = new_node.get("name", None)
             node.value = new_node["value"]
         elif node.node_type == TreeNodeType.INPUT:
             new_node = self._get_new_node(node)
             node.name = new_node["name"]
+
+    def mutate(self, tree):
+        method = self.config["mutation"]["method"]
+        mutation_prob = self.config["mutation"]["probability"]
+        prob = random()
+
+        if mutation_prob >= prob:
+            if method == "POINT_MUTATION":
+                self.point_mutation(tree)
+            else:
+                raise RuntimeError("Undefined mutation method!")

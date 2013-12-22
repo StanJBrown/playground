b377a4be51816b1eef4f8e10dc92dbb49ec5b0f8
Chris Choi <chutsu@gmail.com>
2013-12-22 14:32:29 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name="playground",
+    version="0.1",
+    description="Genetic Programming Framework",
+    author="Chris Choi",
+    author_email="chutsu@gmail.com",
+    packages=["playground", "playnode"]
+)

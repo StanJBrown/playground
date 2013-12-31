6942913ab1c2882b1d5eea6e46e5dc172f9c1b1a
Chris Choi <chutsu@gmail.com>
2013-12-31 02:58:42 +0000

    Begin working on Cartesian Genetic Programming

---
+++ playground/gp_cartesian/cartesian_decoder.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+
+class CartesianDecoder(object):
+    def __init__(self):
+        pass
+
+    def nodes_to_process(self, matrix, active_nodes):
+        del active_nodes[:]
+        pass
+
+    def decode_cartesian(self):
+        pass

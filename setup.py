df3ad97b28ee8ac664b047453d6968bb4f578b5f
Chris Choi <chutsu@gmail.com>
2014-02-03 17:03:14 +0000

    Update Travis and setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,13 @@
 #!/usr/bin/env python
-from distutils.core import setup
+from setuptools import setup
+from setuptools import find_packages
+
 
 setup(
     name="playground",
     version="0.1",
-    description="Genetic Programming Framework",
+    description="Evoluationary Algorithms Library",
     author="Chris Choi",
     author_email="chutsu@gmail.com",
-    packages=[
-        "playground",
-        "playground.benchmark",
-        "playground.ga",
-        "playground.gp",
-        "playground.gp.tree",
-        "playground.gp.cartesian",
-        "playground.operators",
-        "playground.playnode",
-        "playground.recorder"
-    ]
+    packages=find_packages()
 )

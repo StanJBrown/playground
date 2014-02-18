9c56c3c2ea2c6b60f4f6508e51c64e9db82422f7
Chris Choi <chutsu@gmail.com>
2014-02-18 14:44:50 +0000

    Refactor symreg_benchmark module

--- playground/benchmark/symreg_benchmark.py
+++ playground/benchmark/symreg_benchmark.py
@@ -1,43 +1,43 @@
 #!/usr/bin/env python
-import sys
 import os
+import sys
 import time
+import json
 import random
 sys.path.append(os.path.join(os.path.dirname(__file__), "../../"))
 
 # import playground.config as config
 import playground.play as play
+from playground.config import load_config
 from playground.gp.tree.tree_generator import TreeGenerator
 from playground.gp.tree.tree_evaluation import evaluate
+from playground.gp.tree.tree_evaluation import default_stop_func
+# from playground.gp.tree.tree_evaluation import print_func
 from playground.functions import FunctionRegistry
 from playground.operators.selection import Selection
 from playground.operators.crossover import GPTreeCrossover
 from playground.operators.mutation import GPTreeMutation
 from playground.recorder.json_store import JSONStore
-
-import playground.benchmark.symreg_test_funcs as test_funcs
+from playground.parameter_setter.tuner import naive_parameter_sweep
 
 # SETTINGS
 record_exception = False
 script_path = os.path.dirname(os.path.realpath(__file__))
-config_fp = os.path.join(script_path, "config.json")
+config_fp = os.path.join(script_path, "config", "config.json")
 
 
-def benchmark_arabas_et_al_test_functions():
-    # generate test data
-    test_funcs.arabas_et_al_test_functions(script_path)
-
-    pass
+def benchmark_naive_parameter_sweep(config):
+    naive_parameter_sweep(config, benchmark_loop_gp_tree)
 
 
-def benchmark_nguyen_et_al_test_functions():
-    # generate test data
-    test_funcs.nguyen_et_al_test_functions(script_path)
-
-    pass
-
-
-def benchmark_loop(config):
+def benchmark_loop_gp_tree(config):
+    print(
+        "RUN -> pop size: {0} crossover prob: {1} mutation prob: {2}".format(
+            config["max_population"],
+            config["crossover"]["probability"],
+            config["mutation"]["probability"]
+        )
+    ),
     try:
         # setup
         random.seed(config["random_seed"])  # VERY IMPORTANT!
@@ -54,31 +54,78 @@ def benchmark_loop(config):
         population = tree_generator.init()
 
         start_time = time.time()
-        details = {
-            "population": population,
-            "functions": functions,
-            "evaluate": evaluate,
-            "selection": selection,
-            "crossover": crossover,
-            "mutation": mutation,
-            "config": config,
-            "recorder": json_store
-        }
+        details = play.play_details(
+            population=population,
+            functions=functions,
+            evaluate=evaluate,
+            selection=selection,
+            crossover=crossover,
+            mutation=mutation,
+            stop_func=default_stop_func,
+            # print_func=print_func,
+            config=config,
+            recorder=json_store
+        )
 
         play.play(details)
         end_time = time.time()
 
-        print("GP run took: %2.2fsecs\n" % (end_time - start_time))
+        print(
+            "pop size: {0} crossover prob: {1} mutation prob: {2}".format(
+                config["max_population"],
+                config["crossover"]["probability"],
+                config["mutation"]["probability"]
+            )
+        ),
+        print(" [run took: %2.2fsecs]" % (end_time - start_time))
 
-    except Exception as err:
-        print err
+    except Exception as err_msg:
+        print err_msg
 
         # write exception out
-        if record_exception:
-            exception_file = open("/tmp/symbolic_regression_err.log", "w")
-            exception_file.write(str(err) + "\n")
-            exception_file.close()
+        err = open(
+            "/tmp/play-{0}-{1}-{2}-{3}.err".format(
+                config["max_population"],
+                config["crossover"]["probability"],
+                config["mutation"]["probability"],
+                config["random_seed"],
+            ),
+            "w+"
+        )
+        err.write(str(err_msg) + "\n\n")
+        err.write(json.dumps(config))
+        err.close()
 
 
 if __name__ == "__main__":
-    pass
+    config = load_config(config_fp, script_path)
+
+    param_config = {
+        "play_config": config,
+        "iterations": 1,
+
+        "population_size": {
+            "range": [
+                # 10, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000
+                # 10, 20
+            ]
+        },
+
+        "crossover_probability": {
+            "range": [
+                # 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0
+                # 0.1, 0.2
+            ]
+        },
+
+        "mutation_probability": {
+            "range": [
+                # 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0
+                # 0.1, 0.2
+            ]
+        },
+
+        "record_dir": "/tmp/data"
+    }
+
+    benchmark_naive_parameter_sweep(param_config)

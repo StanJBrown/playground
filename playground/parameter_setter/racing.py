7084d3ad41337e56fa7968ddbe433dedc7d84124
Chris Choi <chutsu@gmail.com>
2014-03-31 18:15:57 +0100

    Add notes for f-racing

--- playground/parameter_setter/racing.py
+++ playground/parameter_setter/racing.py
@@ -58,3 +58,47 @@ def friedman_test_no_ties(data):
     p_value = 1 - stats.chi2.cdf(test_stats, dof)
 
     return (test_stats, p_value)
+
+# parameterized algorithm can be formally defined as 7 tuple
+# - infinite set of candidate configurations
+# - infinite set of instances
+# - probability measure over the set I
+# - function associating to every instance the computation time allocated
+# - random variable representing cost measure of a config on specific instance
+# - range of c that is the possible values for the cost measure of the config
+# on instance
+# - probability measure over the set C
+# - measures in some sense the desirability of specific config
+# - total amount of time available for experimenting with given candidate
+# config
+
+# brute-froce problems:
+# - too few configurations
+# - too many configurations
+# - spends equal amount of time on bad and good configurations
+#
+# racing-algorithm:
+# - racing algorithm mitigates the last point
+# - the problem of using racing algorithm approach is it **may not** mitigate
+# the total amount of time needed to find best configuration
+#
+# - if null hypo rejected, we are justified in using pairwise comparisons. All
+# candidates *significantly* worst (student t-test dist) then the best are
+# discarded.
+# - else all candidates remain
+#
+# friedman test requires n >= 10, minimal repeated measurements 6
+
+
+# def f_race(param):
+#     configurations = param["configurations"]
+#     test_instances = param["test_instances"]
+#
+#     step = 0
+#     survived_configs = []
+
+
+
+
+
+

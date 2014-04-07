2b3eb5be493a4ba9e1ffe5ebf52a09798eed6c79
Chris Choi <chutsu@gmail.com>
2014-04-07 17:31:48 +0100

    Add Analyzer implementation and tests

--- playground/benchmark/analyzer.py
+++ playground/benchmark/analyzer.py
@@ -1,240 +1,206 @@
 #!/usr/bin/env python
 import os
 import json
+import zipfile
 
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 
-def parse_data_file(data_fp):
+def parse_data(fp):
     generations = []
 
-    data_file = open(data_fp, "r")
-    for line in data_file:
-        generations.append(json.loads(line))
+    try:
+        # parse fp
+        data_file, file_ext = os.path.splitext(fp)
+        dirname = os.path.dirname(fp)
+
+        # unpack zip file
+        zipped_data = False
+        if file_ext == ".zip":
+            zipped_data = True
+            with zipfile.ZipFile(fp) as zf:
+                zf.extractall(dirname)
+
+        # parse data - assumes data file has extension .dat
+        data = open(data_file + ".dat", "r")
+        for line in data:
+            generations.append(json.loads(line))
+
+        # remove extraced zip file
+        if zipped_data:
+            os.remove(data_file + ".dat")
+
+    except IOError as msg:
+        print "ERROR: parse_data() assumes data file has extension .dat!"
+        print msg
+        raise
 
     return generations
 
 
-def parse_generation_best_scores(generations):
-    best_scores = []
-
-    for g in generations:
-        best_scores.append(g["population"]["best_score"])
-
-    return best_scores
+def summarize_population(population, summary):
+    summary["generation"].append(population["generation"])
+    summary["best_score"].append(population["best_score"])
+    summary["best_individual"].append(population["best_individual"])
 
 
-def parse_generation_best_individuals(generations):
-    best_individuals = []
-
-    for g in generations:
-        best_individuals.append(g["population"]["best_individuals"])
-
-    return best_individuals
-
-
-def parse_all_time_best_scores(generations, reverse=False):
-    best_scores = []
-    curr_best = None
-    best = generations[0]["population"]["best_score"]
-
-    for g in generations:
-        curr_best = g["population"]["best_score"]
-
-        if reverse:
-            if curr_best > best:
-                best = curr_best
-        else:
-            if curr_best < best:
-                best = curr_best
-        best_scores.append(best)
-
-    return best_scores
+def summarize_evaluation(evaluation, summary):
+    summary["cache_size"].append(evaluation["cache_size"])
+    summary["diversity"].append(evaluation["diversity"])
+    summary["matched_cache"].append(evaluation["match_cached"])
+    summary["trees_evaluated"].append(evaluation["trees_evaluated"])
+    summary["tree_nodes_evaluated"].append(evaluation["tree_nodes_evaluated"])
 
 
-def parse_all_time_best_individuals(generations, reverse=False):
-    best_individuals = []
-    curr_best_score = None
-    curr_best_individual = None
-    best_score = generations[0]["population"]["best_score"]
-    best_individual = generations[0]["population"]["best_individual"]
+def summarize_crossover(crossover, summary):
+    for key in crossover.iterkeys():
+        if key == "crossovers":
+            summary[key].append(crossover[key])
 
-    for g in generations:
-        curr_best_score = g["population"]["best_score"]
-        curr_best_individual = g["population"]["best_individual"]
+        elif key == "no_crossovers":
+            summary[key].append(crossover[key])
 
-        if reverse:
-            if curr_best_score > best_score:
-                best_score = curr_best_score
-                best_individual = curr_best_individual
-        else:
-            if curr_best_score < best_score:
-                best_score = curr_best_score
-                best_individual = curr_best_individual
-        best_individuals.append(best_individual)
+        elif key not in summary:
+            summary[key] = {}
+            summary[key]["frequency"] = []
+            summary[key]["failed"] = []
+            summary[key]["success"] = []
 
-    return best_individuals
+        if key != "crossovers" and key != "no_crossovers":
+            success = crossover[key]["success"]
+            failed = crossover[key]["failed"]
+            frequency = crossover[key]["frequency"]
+
+            summary[key]["success"].append(success)
+            summary[key]["failed"].append(failed)
+            summary[key]["frequency"].append(frequency)
 
 
-def parse_evaluation_stats(generations):
-    cache_size = []
-    matched_cache = []
-    trees_evaluated = []
-    result = {
-        "cache_size": cache_size,
-        "matched_cache": matched_cache,
-        "trees_evaluated": trees_evaluated
+def summarize_mutation(mutation, summary):
+    for key in mutation.iterkeys():
+        if key == "mutations":
+            summary[key].append(mutation[key])
+
+        elif key == "no_mutations":
+            summary[key].append(mutation[key])
+
+        elif key not in summary:
+            summary[key] = {}
+            summary[key]["frequency"] = []
+            summary[key]["failed"] = []
+            summary[key]["success"] = []
+
+        if key != "mutations" and key != "no_mutations":
+            success = mutation[key]["success"]
+            failed = mutation[key]["failed"]
+            frequency = mutation[key]["frequency"]
+
+            summary[key]["success"].append(success)
+            summary[key]["failed"].append(failed)
+            summary[key]["frequency"].append(frequency)
+
+
+def summarize_selection(selection, summary):
+    sel_method = selection["method"]
+    if sel_method not in summary:
+        summary[sel_method] = {}
+        summary[sel_method]["selected"] = []
+
+    summary[sel_method]["selected"].append(selection["selected"])
+
+
+def summarize_generations(generations):
+    stats = {
+        # population
+        "population": {
+            "generation": [],
+            "best_score": [],
+            "best_individual": []
+        },
+
+        # evaluation
+        "evaluation": {
+            "cache_size": [],
+            "diversity": [],
+            "matched_cache": [],
+            "trees_evaluated": [],
+            "tree_nodes_evaluated": []
+        },
+
+        # crossover
+        "crossover": {
+            "crossovers": [],
+            "no_crossovers": []
+        },
+
+        # mutation
+        "mutation": {
+            "mutations": [],
+            "no_mutations": []
+        },
+
+        # selection
+        "selection": {},
     }
 
-    import pprint
-    pprint.pprint(generations[0]["population"])
-    for g in generations:
-        cache_size.append(g["evaluation"]["cache_size"])
-        matched_cache.append(g["evaluation"]["match_cached"])
-        trees_evaluated.append(g["evaluation"]["trees_evaluated"])
+    for generation in generations:
+        summarize_population(generation["population"], stats["population"])
+        summarize_selection(generation["selection"], stats["selection"])
+        summarize_crossover(generation["crossover"], stats["crossover"])
+        summarize_mutation(generation["mutation"], stats["mutation"])
 
-    return result
+    return stats
 
-
-def plot_generation_best_scores(generations, **kwargs):
-    font = {
-        "family": "san_serif",
-        "color": "black",
-        "weight": "bold",
-        "size": 12
-    }
-
-    # graph data
-    best_scores = parse_generation_best_scores(generations)
-    gens = range(0, len(best_scores))
-
-    # plot graph
-    plt.plot(gens, best_scores)
-
-    # graph labels
-    plt.title("Generation Best Score vs Generations", fontdict=font)
-    plt.xlabel("Generation", fontdict=font)
-    plt.ylabel("Generation Best Score", fontdict=font)
-
-    if kwargs.get("show_graph", False):
-        # show graph
-        plt.show()
-
-    if kwargs.get("save_fig", False):
-        # save graph
-        plt.savefig(kwargs.get("fig_path", "graph.png"))
+# def plot_generations(generations, **kwargs):
+#     font = {
+#         "family": "san_serif",
+#         "color": "black",
+#         "weight": "bold",
+#         "size": 12
+#     }
+#
+#     # graph data
+#     best_scores = parse_generation_best_scores(generations)
+#     gens = range(0, len(best_scores))
+#
+#     # plot graph
+#     plt.plot(gens, best_scores)
+#
+#     # graph labels
+#     plt.title("Generation Best Score vs Generations", fontdict=font)
+#     plt.xlabel("Generation", fontdict=font)
+#     plt.ylabel("Generation Best Score", fontdict=font)
+#
+#     if kwargs.get("show_graph", False):
+#         # show graph
+#         plt.show()
+#
+#     if kwargs.get("save_fig", False):
+#         # save graph
+#         plt.savefig(kwargs.get("fig_path", "graph.png"))
 
 
-def plot_all_time_best_scores(generations, **kwargs):
-    font = {
-        "family": "san_serif",
-        "color": "black",
-        "weight": "bold",
-        "size": 12
-    }
-
-    # graph data
-    best_scores = parse_all_time_best_scores(generations)
-    gens = range(0, len(best_scores))
-
-    # plot graph
-    plt.plot(gens, best_scores)
-
-    # graph labels
-    plt.title("Best Score vs Generations", fontdict=font)
-    plt.xlabel("Generation", fontdict=font)
-    plt.ylabel("Best Score", fontdict=font)
-
-    if kwargs.get("show_graph", False):
-        # show graph
-        plt.show()
-
-    if kwargs.get("save_fig", False):
-        # save graph
-        plt.savefig(kwargs.get("fig_path", "graph.png"))
-
-
-def plot_evaluation_stats(generations, **kwargs):
-    font = {
-        "family": "san_serif",
-        "color": "black",
-        "weight": "bold",
-        "size": 12
-    }
-
-    # graph data
-    stats = parse_evaluation_stats(generations)
-    gens = range(0, len(stats["cache_size"]))
-
-    # plot graph
-    plt.plot(gens, stats["cache_size"])
-    plt.plot(gens, stats["matched_cache"])
-    plt.plot(gens, stats["trees_evaluated"])
-
-    # axis
-    max_y = max(
-        [
-            max(stats["cache_size"]),
-            max(stats["matched_cache"]),
-            max(stats["trees_evaluated"])
-        ]
-    )
-    plt.ylim([0, max_y + (max_y * 0.1)])
-
-    # graph labels
-    plt.title("Evauation Statistics", fontdict=font)
-    plt.xlabel("Generation", fontdict=font)
-    plt.legend(
-        ["cache size", "matched cache", "evaluated"],
-        loc='upper right',
-        prop={'size': 8}
-    )
-
-    if kwargs.get("show_graph", False):
-        # show graph
-        plt.show()
-
-    if kwargs.get("save_fig", False):
-        # save graph
-        plt.savefig(kwargs.get("fig_path", "graph.png"))
-
-
-def plot_summary(generations):
-    plt.figure(figsize=(7, 10))
-
-    # generation best scores
-    plt.subplot(311)
-    plot_generation_best_scores(generations)
-
-    # all time best scores
-    plt.subplot(312)
-    plot_all_time_best_scores(generations)
-
-    # evaluation stats
-    plt.subplot(313)
-    plot_evaluation_stats(generations)
-
-    plt.subplots_adjust(
-        left=None,
-        bottom=None,
-        right=None,
-        top=None,
-        wspace=None,
-        hspace=0.6
-    )
-    plt.show()
-
-
-if __name__ == "__main__":
-    # data_dir = "/tmp/data"
-    # data_file = "np_sweep_10_0.1_0.2-0.dat"
-    # data_path = os.path.join(data_dir, data_file)
-    data_path = "/tmp/ea_stats.json"
-
-    generations = parse_data_file(data_path)
-
-    # plot_evaluation_stats(generations)
-    # plot_all_time_best_scores(generations, show_graph=True)
-    # plot_generation_best_scores(generations, show_graph=True)
-
-    plot_summary(generations)
+# def plot_summary(generations):
+#     plt.figure(figsize=(7, 10))
+#
+#     # generation best scores
+#     plt.subplot(311)
+#     plot_generation_best_scores(generations)
+#
+#     # all time best scores
+#     plt.subplot(312)
+#     plot_all_time_best_scores(generations)
+#
+#     # evaluation stats
+#     plt.subplot(313)
+#     plot_evaluation_stats(generations)
+#
+#     plt.subplots_adjust(
+#         left=None,
+#         bottom=None,
+#         right=None,
+#         top=None,
+#         wspace=None,
+#         hspace=0.6
+#     )
+#     plt.show()
---
+++ tests/benchmark/analyzer_tests.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+import os
+import sys
+import unittest
+# import time
+sys.path.append(os.path.join(os.path.dirname(__file__), "../../"))
+
+import playground.benchmark.analyzer as analyzer
+
+# SETTINGS
+cwd = os.path.dirname(__file__)
+log_file = os.path.normpath(os.path.join(cwd, "../data/test.log"))
+data_file = "../data/bps--arabas_et_al-f1--100_0.05_0.05-0.zip"
+data_file = os.path.join(cwd, data_file)
+data_file = os.path.normpath(data_file)
+
+
+class AnalzyerTests(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_parse_datafile(self):
+        # pass test
+        result = analyzer.parse_data(data_file)
+        solution = {
+            "population": {
+                "generation": 0,
+                "best_individual": "(COS((var1 MUL 0.0)))",
+                "best_score": 133.6826393201821
+            }
+        }
+        self.assertEquals(result[0]["population"], solution["population"])
+
+        # exception test
+        self.assertRaises(IOError, analyzer.parse_data, log_file)
+
+    def test_summarize_generations(self):
+        # pass test
+        data = analyzer.parse_data(data_file)
+
+        # import pprint
+        # print "---> DATA: ",
+        # pprint.pprint(data[0])
+
+        result = analyzer.summarize_generations(data)
+
+        # import pprint
+        # pprint.pprint(result)
+
+        self.assertIsNotNone(result)
+        self.assertTrue(len(result["population"]["generation"]), 11)
+        self.assertTrue(len(result["population"]["best_score"]), 11)
+        self.assertTrue(len(result["population"]["best_individual"]), 11)
+
+        self.assertTrue(len(result["crossover"]["crossovers"]), 11)
+        self.assertTrue(len(result["crossover"]["no_crossovers"]), 11)
+
+        self.assertTrue(len(result["mutation"]["mutations"]), 11)
+        self.assertTrue(len(result["mutation"]["no_mutations"]), 11)
+
+
+if __name__ == "__main__":
+    unittest.main()

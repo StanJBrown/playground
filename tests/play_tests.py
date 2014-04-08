231712516b9efa152674317cde8d492594a89f3a
Chris Choi <chutsu@gmail.com>
2014-04-08 17:11:46 +0100

    Add play test coverage

--- tests/play_tests.py
+++ tests/play_tests.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import sys
 import os
+import time
+import random
 import unittest
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
@@ -10,6 +12,8 @@ from playground.selection import Selection
 from playground.gp.functions import GPFunctionRegistry
 from playground.gp.tree.tree_generator import TreeGenerator
 from playground.gp.tree.tree_evaluation import evaluate
+from playground.gp.tree.tree_evaluation import default_stop_func
+from playground.gp.tree.tree_evaluation import print_func
 from playground.gp.tree.tree_crossover import TreeCrossover
 from playground.gp.tree.tree_mutation import TreeMutation
 
@@ -20,8 +24,7 @@ config_fp = os.path.normpath(os.path.join(cwd, "config/play.json"))
 
 class PlayTests(unittest.TestCase):
     def setUp(self):
-        # random.seed(10)
-
+        random.seed(0)
         self.config = config.load_config(config_fp)
         self.functions = GPFunctionRegistry()
         self.tree_generator = TreeGenerator(self.config)
@@ -79,92 +82,93 @@ class PlayTests(unittest.TestCase):
             self.assertTrue(population.config is self.config)
             self.assertEquals(population.generation, 0)
 
-    # def test_play(self):
-    #     population = self.tree_generator.init()
+    def test_play(self):
+        population = self.tree_generator.init()
 
-    #     # with cache
-    #     start_time = time.time()
-    #     details = play.play_details(
-    #         population=population,
-    #         functions=self.functions,
-    #         evaluate=evaluate,
-    #         selection=self.selection,
-    #         crossover=self.crossover,
-    #         mutation=self.mutation,
-    #         stop_func=default_stop_func,
-    #         print_func=print_func,
-    #         config=self.config,
-    #         recorder=None
-    #     )
-    #     play.play(details)
-    #     end_time = time.time()
-    #     print("GP run with cache: %2.2fsec\n" % (end_time - start_time))
+        # with cache
+        start_time = time.time()
+        details = play.play_details(
+            population=population,
+            functions=self.functions,
+            evaluate=evaluate,
+            selection=self.selection,
+            crossover=self.crossover,
+            mutation=self.mutation,
+            stop_func=default_stop_func,
+            print_func=print_func,
+            config=self.config,
+            recorder=None
+        )
+        play.play(details)
+        end_time = time.time()
+        print("GP run with cache: %2.2fsec\n" % (end_time - start_time))
 
-    #     # without cache
-    #     start_time = time.time()
-    #     details = play.play_details(
-    #         population=population,
-    #         functions=self.functions,
-    #         evaluate=evaluate,
-    #         selection=self.selection,
-    #         crossover=self.crossover,
-    #         mutation=self.mutation,
-    #         stop_func=default_stop_func,
-    #         print_func=print_func,
-    #         config=self.config,
-    #         recorder=None
-    #     )
-    #     play.play(details)
-    #     end_time = time.time()
-    #     print("GP run without cache: %2.2fsec\n" % (end_time - start_time))
+        # without cache
+        start_time = time.time()
+        details = play.play_details(
+            population=population,
+            functions=self.functions,
+            evaluate=evaluate,
+            selection=self.selection,
+            crossover=self.crossover,
+            mutation=self.mutation,
+            stop_func=default_stop_func,
+            print_func=print_func,
+            config=self.config,
+            recorder=None
+        )
+        play.play(details)
+        end_time = time.time()
+        print("GP run without cache: %2.2fsec\n" % (end_time - start_time))
 
-    #     # assert
-    #     self.assertTrue(len(population.individuals) > 0)
+        # assert
+        self.assertTrue(len(population.individuals) > 0)
 
-    # def test_play_multicore(self):
-    #     population = self.tree_generator.init()
+    def test_play_multicore(self):
+        population = self.tree_generator.init()
 
-    #     start_time = time.time()
-    #     details = play.play_details(
-    #         population=population,
-    #         functions=self.functions,
-    #         evaluate=evaluate,
-    #         selection=self.selection,
-    #         crossover=self.crossover,
-    #         mutation=self.mutation,
-    #         stop_func=default_stop_func,
-    #         print_func=print_func,
-    #         config=self.config,
-    #         recorder=None
-    #     )
-    #     play.play_multicore(details)
-    #     end_time = time.time()
-    #     print("GP run without cache: %2.2fsec\n" % (end_time - start_time))
+        start_time = time.time()
+        details = play.play_details(
+            population=population,
+            functions=self.functions,
+            evaluate=evaluate,
+            selection=self.selection,
+            crossover=self.crossover,
+            mutation=self.mutation,
+            stop_func=default_stop_func,
+            print_func=print_func,
+            config=self.config,
+            recorder=None
+        )
+        play.play_multicore(details)
+        end_time = time.time()
+        print("GP run without cache: %2.2fsec\n" % (end_time - start_time))
 
-    #     # assert
-    #     self.assertTrue(len(population.individuals) > 0)
+        # assert
+        self.assertTrue(len(population.individuals) > 0)
 
-    # def test_play_evolution_strategy(self):
-    #     population = self.tree_generator.init()
-    #     self.config["max_population"] = 4
+    def test_play_evolution_strategy(self):
+        population = self.tree_generator.init()
+        self.config["max_population"] = 4
 
-    #     start_time = time.time()
-    #     details = play.play_details(
-    #         population=population,
-    #         functions=self.functions,
-    #         evaluate=evaluate,
-    #         mutation=self.mutation,
-    #         stop_func=default_stop_func,
-    #         print_func=print_func,
-    #         config=self.config
-    #     )
-    #     play.play_evolution_strategy(details)
-    #     end_time = time.time()
-    #     print("GP run without cache: %2.2fsec\n" % (end_time - start_time))
+        start_time = time.time()
+        details = play.play_details(
+            population=population,
+            functions=self.functions,
+            evaluate=evaluate,
+            mutation=self.mutation,
+            stop_func=default_stop_func,
+            print_func=print_func,
+            config=self.config
+        )
+        play.play_evolution_strategy(details)
+        end_time = time.time()
+        print("GP run without cache: %2.2fsec\n" % (end_time - start_time))
 
-    #     # assert
-    #     self.assertTrue(len(population.individuals) > 1)
-    #     # because 1 or more individual may have evaluation error
+        # assert
+        print len(population.individuals)
+        self.assertTrue(len(population.individuals) > 1)
+        # because 1 or more individual may have evaluation error
 
 
 if __name__ == "__main__":

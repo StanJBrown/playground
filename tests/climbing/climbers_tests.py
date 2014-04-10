3904162804ed2c73b09805e82f8f02fd6a13a305
Chris Choi <chutsu@gmail.com>
2014-04-10 17:08:48 +0100

    Add hill_climber() test coverage

---
+++ tests/climbing/climbers_tests.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+import os
+import sys
+import random
+import unittest
+sys.path.append(os.path.join(os.path.dirname(__file__), "../../"))
+
+from playground.climbing.climbers import hill_climbing
+from playground.climbing.climbers import default_stop_function
+
+
+class ClimbersTests(unittest.TestCase):
+    def setUp(self):
+        self.solution = "helloworld"
+        self.candidate = "abcdefghij"
+
+        self.hill_climbing_details = {
+            "debug": False,
+
+            "tweak_function": self.tweak_function,
+            "eval_function": self.eval_function,
+            "stop_function": default_stop_function,
+
+            "candidate": self.candidate,
+            "max_iterations": 1000,
+            "target_score": 1000,
+
+            "comparator": self.int_cmp
+        }
+
+    def tearDown(self):
+        pass
+
+    def eval_function(self, candidate):
+        solution = list("helloworld")
+        test = list(candidate)
+
+        # calcuate character difference between two strings
+        dist_err = 0
+        for i in range(len(test)):
+            dist_err += abs(ord(test[i]) - ord(solution[i]))
+
+        return 1000 - dist_err
+
+    def tweak_function(self, candidate):
+        # make a copy of candidate to be tweaked
+        tweak_candidate = list(candidate)
+
+        # randomly select a character to be tweak
+        index = random.randint(0, len(candidate) - 1)
+
+        # randomly tweak up or down
+        if random.random() > 0.5:
+            tweak_candidate[index] = chr(ord(candidate[index]) + 1)
+        else:
+            tweak_candidate[index] = chr(ord(candidate[index]) - 1)
+
+        return "".join(tweak_candidate)
+
+    def int_cmp(self, num_1, num_2):
+        if num_1 > num_2:
+            return 1
+        elif num_1 < num_2:
+            return -1
+        else:
+            return 0
+
+    def test_hill_climbing(self):
+        # run hill climbing
+        result = hill_climbing(self.hill_climbing_details)
+
+        # assert
+        self.assertEquals(result[0], self.solution)
+        self.assertEquals(result[1], 1000)
+
+if __name__ == "__main__":
+    unittest.main()

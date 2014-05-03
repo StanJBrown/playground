d086d3bb77a7687f7545e2d223ef5939a6bd9957
Chris Choi <chutsu@gmail.com>
2014-05-03 15:33:48 +0100

    Mod color_observer script to class

--- examples/observer/color_observer.py
+++ examples/observer/color_observer.py
@@ -3,174 +3,189 @@ import cv2
 import numpy as np
 
 
-def hsv_callback(val):
-    pass
+class ColorDetector(object):
+    def __init__(self, **kwargs):
+        CV_CAP_PROP_FRAME_WIDTH = 3
+        CV_CAP_PROP_FRAME_HEIGHT = 4
 
+        # video frame dimensions
+        width = kwargs.get("video_width", 640)
+        height = kwargs.get("video_height", 480)
 
-def create_hsv_gui(hsv_vals):
-    window = "hsv"
-    cv2.namedWindow(window)
+        # initial HSV values
+        hsv_vals = (
+            20,  # hue_min
+            0,  # sat_min
+            0,  # val_min
+            75,  # hue_max
+            255,  # sat_max
+            255,  # val_max
+        )
 
-    cv2.createTrackbar("Hue Min", window, hsv_vals[0], 255, hsv_callback)
-    cv2.createTrackbar("Hue Max", window, hsv_vals[3], 255, hsv_callback)
+        # find and initialize the webcam
+        self.capture = cv2.VideoCapture(0)
+        self.capture.set(CV_CAP_PROP_FRAME_WIDTH, width)
+        self.capture.set(CV_CAP_PROP_FRAME_HEIGHT, height)
 
-    cv2.createTrackbar("Sat Min", window, hsv_vals[1], 255, hsv_callback)
-    cv2.createTrackbar("Sat Max", window, hsv_vals[4], 255, hsv_callback)
+        # create gui
+        self.create_hsv_gui(hsv_vals)
 
-    cv2.createTrackbar("Val Min", window, hsv_vals[2], 255, hsv_callback)
-    cv2.createTrackbar("Val Max", window, hsv_vals[5], 255, hsv_callback)
+        # detector states
+        self.detection_area = False
+        self.get_hsv = False
 
+    def hsv_callback(self, val):
+        pass
 
-def draw_detection_area(video_frame):
-    center_x = video_frame.shape[1] / 2
-    center_y = video_frame.shape[0] / 2
+    def create_hsv_gui(self, hsv_vals):
+        win = "hsv"
+        cv2.namedWindow(win)
 
-    area_size = 10
-    top_left = (center_x - area_size, center_y - area_size)
-    bottom_right = (center_x + area_size, center_y + area_size)
+        # HUE
+        cv2.createTrackbar("Hue Min", win, hsv_vals[0], 255, self.hsv_callback)
+        cv2.createTrackbar("Hue Max", win, hsv_vals[3], 255, self.hsv_callback)
 
-    cv2.rectangle(video_frame, top_left, bottom_right, (0, 0, 255), 3)
-    return video_frame
+        # SAT
+        cv2.createTrackbar("Sat Min", win, hsv_vals[1], 255, self.hsv_callback)
+        cv2.createTrackbar("Sat Max", win, hsv_vals[4], 255, self.hsv_callback)
 
+        # VAL
+        cv2.createTrackbar("Val Min", win, hsv_vals[2], 255, self.hsv_callback)
+        cv2.createTrackbar("Val Max", win, hsv_vals[5], 255, self.hsv_callback)
 
-def pixel_get_hsv(video_frame):
-    center_x = video_frame.shape[1] / 2
-    center_y = video_frame.shape[0] / 2
+    def draw_detection_area(self, video_frame):
+        # get video frame center
+        center_x = video_frame.shape[1] / 2
+        center_y = video_frame.shape[0] / 2
 
-    hsv_frame = cv2.cvtColor(video_frame, cv2.COLOR_BGR2HSV)
-    hsv = hsv_frame[center_y][center_x]
-    hue = hsv[0]
-    sat = hsv[1]
-    val = hsv[2]
+        # draw a small red rectange in the middle
+        area_size = 10
+        top_left = (center_x - area_size, center_y - area_size)
+        bottom_right = (center_x + area_size, center_y + area_size)
+        cv2.rectangle(video_frame, top_left, bottom_right, (0, 0, 255), 3)
 
-    print hue, sat, val
+        return video_frame
 
-    return (hue, sat, val)
+    def pixel_get_hsv(self, video_frame):
+        # get video frame center
+        center_x = video_frame.shape[1] / 2
+        center_y = video_frame.shape[0] / 2
 
+        # get hsv pixel value from the center of the video frame
+        hsv_frame = cv2.cvtColor(video_frame, cv2.COLOR_BGR2HSV)
+        hsv = hsv_frame[center_y][center_x]
+        hue = hsv[0]
+        sat = hsv[1]
+        val = hsv[2]
+        print "PIXEL HSV:", hue, sat, val
 
-def update_hsv_gui(hsv, hue_range=10):
-    hue, sat, val = hsv
-    hue_min = hue - hue_range if hue - hue_range > 0 else 0
-    hue_max = hue + hue_range if hue + hue_range < 255 else 255
-    sat_min = sat
-    sat_max = 255
-    val_min = val
-    val_max = 255
+        return (hue, sat, val)
 
-    cv2.setTrackbarPos("Hue Min", "hsv", hue_min)
-    cv2.setTrackbarPos("Hue Max", "hsv", hue_max)
+    def update_hsv_gui(self, hsv, hue_range=10):
+        hue, sat, val = hsv
 
-    cv2.setTrackbarPos("Sat Min", "hsv", sat_min)
-    cv2.setTrackbarPos("Sat Max", "hsv", sat_max)
+        hue_min = hue - hue_range if hue - hue_range > 0 else 0
+        hue_max = hue + hue_range if hue + hue_range < 255 else 255
+        sat_min = sat
+        sat_max = 255
+        val_min = val
+        val_max = 255
 
-    cv2.setTrackbarPos("Val Min", "hsv", val_min)
-    cv2.setTrackbarPos("Val Max", "hsv", val_max)
+        cv2.setTrackbarPos("Hue Min", "hsv", hue_min)
+        cv2.setTrackbarPos("Hue Max", "hsv", hue_max)
 
+        cv2.setTrackbarPos("Sat Min", "hsv", sat_min)
+        cv2.setTrackbarPos("Sat Max", "hsv", sat_max)
 
-def detect(orig_frame, thres_frame, contour_area=300):
-    kernel = np.ones((5, 5), np.uint8)
-    dilate = cv2.dilate(thres_frame, kernel, iterations=1)
-    contours, hierarchy = cv2.findContours(
-        dilate,
-        cv2.RETR_TREE,
-        cv2.CHAIN_APPROX_SIMPLE
-    )
+        cv2.setTrackbarPos("Val Min", "hsv", val_min)
+        cv2.setTrackbarPos("Val Max", "hsv", val_max)
 
-    # loop through discovered contour
-    detected = []
-    for i in range(len(contours)):
-        if cv2.contourArea(contours[i]) > contour_area:
-            # obtain contour center and append to detected
-            M = cv2.moments(contours[i])
-            cx = int(M["m10"] / M["m00"])
-            cy = int(M["m01"] / M["m00"])
-            detected.append((cx, cy))
+    def detect_color(self, orig_frame, thres_frame, contour_area=300):
+        kernel = np.ones((5, 5), np.uint8)
+        dilate = cv2.dilate(thres_frame, kernel, iterations=1)
+        contours, hierarchy = cv2.findContours(
+            dilate,
+            cv2.RETR_TREE,
+            cv2.CHAIN_APPROX_SIMPLE
+        )
 
-            # draw detected contour
-            cv2.drawContours(orig_frame, contours, i, (0, 255, 0), 3)
+        # loop through discovered contour
+        detected = []
+        for i in range(len(contours)):
+            if cv2.contourArea(contours[i]) > contour_area:
+                # obtain contour center and append to detected
+                M = cv2.moments(contours[i])
+                cx = int(M["m10"] / M["m00"])
+                cy = int(M["m01"] / M["m00"])
+                detected.append((cx, cy))
 
-            # draw rectangle
-            x, y, w, h = cv2.boundingRect(contours[i])
-            cv2.rectangle(orig_frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                # draw detected contour
+                cv2.drawContours(orig_frame, contours, i, (0, 255, 0), 3)
 
-    cv2.imshow("Detected", orig_frame)
+                # draw rectangle
+                x, y, w, h = cv2.boundingRect(contours[i])
+                origin = (x, y)
+                bounds = (x + w, y + h)
+                cv2.rectangle(orig_frame, origin, bounds, (0, 0, 255), 2)
 
-    return detected
+        cv2.imshow("Detected", orig_frame)
 
+        return detected
 
-def cleanup(capture):
-    capture.release()
-    cv2.destroyAllWindows()
+    def cleanup(self):
+        self.capture.release()
+        cv2.destroyAllWindows()
+
+    def run(self):
+        while (self.capture.isOpened()):
+            ret, frame = self.capture.read()
+            key = cv2.waitKey(50)
+
+            if ret:
+                if key == 27 or key == ord('q'):
+                    self.cleanup()
+                    break
+
+                elif key == ord('d'):
+                    self.detection_area = not self.detection_area
+
+                elif key == ord('h'):
+                    self.get_hsv = not self.get_hsv
+
+                else:
+                    # draw detection area
+                    if self.detection_area:
+                        self.draw_detection_area(frame)
+
+                    # detect hsv value in detection area and update hsv gui
+                    if self.get_hsv:
+                        hsv = self.pixel_get_hsv(frame)
+                        self.update_hsv_gui(hsv)
+
+                    # get hsv values from hsv gui
+                    lbound = (
+                        cv2.getTrackbarPos("Hue Min", "hsv"),
+                        cv2.getTrackbarPos("Sat Min", "hsv"),
+                        cv2.getTrackbarPos("Val Min", "hsv")
+                    )
+
+                    ubound = (
+                        cv2.getTrackbarPos("Hue Max", "hsv"),
+                        cv2.getTrackbarPos("Sat Max", "hsv"),
+                        cv2.getTrackbarPos("Val Max", "hsv")
+                    )
+
+                    # draw video and threshold windows
+                    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+                    thres_frame = cv2.inRange(hsv_frame, lbound, ubound)
+
+                    # cv2.imshow('Video Stream', frame)
+                    cv2.imshow('Threshold', thres_frame)
+                    self.detect_color(frame, thres_frame)
+            else:
+                break
 
 
 if __name__ == "__main__":
-    CV_CAP_PROP_FRAME_WIDTH = 3
-    CV_CAP_PROP_FRAME_HEIGHT = 4
-
-    width = 640
-    height = 480
-
-    hsv_vals = (
-        20,  # hue_min
-        0,  # sat_min
-        0,  # val_min
-        75,  # hue_max
-        255,  # sat_max
-        255,  # val_max
-    )
-
-    # find the webcam
-    capture = cv2.VideoCapture(0)
-    capture.set(CV_CAP_PROP_FRAME_WIDTH, width)
-    capture.set(CV_CAP_PROP_FRAME_HEIGHT, height)
-
-    # create gui
-    create_hsv_gui(hsv_vals)
-
-    # record video
-    detection_area = False
-    get_hsv = False
-    while (capture.isOpened()):
-        ret, frame = capture.read()
-        key = cv2.waitKey(50)
-
-        if ret:
-            if key == 27 or key == ord('q'):
-                cleanup(capture)
-                break
-            elif key == ord('d'):
-                detection_area = not detection_area
-            elif key == ord('h'):
-                get_hsv = not get_hsv
-            else:
-                # draw detection area
-                if detection_area:
-                    draw_detection_area(frame)
-
-                # detect hsv value in detection area and update hsv gui
-                if get_hsv:
-                    hsv = pixel_get_hsv(frame)
-                    update_hsv_gui(hsv)
-
-                # get hsv values from hsv gui
-                lbound = (
-                    cv2.getTrackbarPos("Hue Min", "hsv"),
-                    cv2.getTrackbarPos("Sat Min", "hsv"),
-                    cv2.getTrackbarPos("Val Min", "hsv")
-                )
-
-                ubound = (
-                    cv2.getTrackbarPos("Hue Max", "hsv"),
-                    cv2.getTrackbarPos("Sat Max", "hsv"),
-                    cv2.getTrackbarPos("Val Max", "hsv")
-                )
-
-                # draw video and threshold windows
-                hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-                thres_frame = cv2.inRange(hsv_frame, lbound, ubound)
-
-                cv2.imshow('Video Stream', frame)
-                cv2.imshow('Threshold', thres_frame)
-                detect(frame, thres_frame)
-        else:
-            break
+    detector = ColorDetector()
+    detector.run()
